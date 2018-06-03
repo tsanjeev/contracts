b36b6c18aa3f3c7315acb69783a3b09d7da0d6b4
Ted Sanjeevi <tsanjeev03@gmail.com>
2018-06-03 14:54:14 -0700

    add balance

--- exchange.sol
+++ exchange.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.17;
 //contract address
-//0xe8d98f8c9c2b0ab5e9d0aca26d914a7fc002a4de
+//0x1875833e773fa33cf1e65f38d1c0e26ba6b1cbfb
 
 contract BurgerToken {
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
@@ -15,9 +15,16 @@ contract Exchange {
     mapping ( address => uint256 ) public balanceOf;
     address tokenAddress = 0x5c87e1c456dcedfab139df81a7ca331bb0c14dda;
 
+    address ownerOne = 0x7303efC3B94bB9D7D37511303A5108601D9A82FF;
+    address ownerTwo = 0xd090c8f4601A1b02F49aFD9825C5f393a89543bE;
+    address ownerThree = 0x61CD69d123fe3d89aB7D055B3C6980081F859D2A;
+
+    uint256 public etherBalance = 0;
+
     function exchange() public payable {
         require(msg.value > 1 ether);
         uint256 toEther = msg.value/(1 ether);
+        etherBalance += toEther;
         uint256 etherToBurgerToken = toEther * exchangeRate *  decimals;
         BurgerToken(tokenAddress).transfer(msg.sender, etherToBurgerToken);
         balanceOf[this] -= etherToBurgerToken;
@@ -31,13 +38,27 @@ contract Exchange {
     }
 
     function withdrawEther(address _to) public {
+        require(msg.sender == ownerOne ||
+                msg.sender == ownerTwo ||
+                msg.sender == ownerThree);
         _to.transfer(this.balance);
+        etherBalance = 0;
+    }
+
+    function transferBT(address _to, uint256 tokens) public {
+        require(msg.sender == ownerOne ||
+                msg.sender == ownerTwo ||
+                msg.sender == ownerThree);
+                uint256 tokensToTransfer = tokens * decimals;
+                BurgerToken(tokenAddress).transfer(_to, tokensToTransfer);
+                balanceOf[this] -= tokensToTransfer;
+
+
     }
 
 }
 
-/*
-
+/**
 [
   {
     constant: true,
@@ -46,6 +67,36 @@ contract Exchange {
     outputs: [{ name: "", type: "uint256" }],
     payable: false,
     stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "etherBalance",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [{ name: "_to", type: "address" }],
+    name: "withdrawEther",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: "_to", type: "address" },
+      { name: "tokens", type: "uint256" }
+    ],
+    name: "transferBT",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
     type: "function"
   },
   {
@@ -66,5 +117,4 @@ contract Exchange {
     stateMutability: "payable",
     type: "function"
   }
-]
-*/
+];*/
